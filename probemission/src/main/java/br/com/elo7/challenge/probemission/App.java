b1cbec32c5468d984453792af02e455782e7cbb0
lexfadelli <leandro.fadelli@hotmail.com>
2018-01-07 21:36:11 -0200

    bootstrapping

---
+++ probemission/src/main/java/br/com/elo7/challenge/probemission/App.java
@@ -0,0 +1,13 @@
+package br.com.elo7.challenge.probemission;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
